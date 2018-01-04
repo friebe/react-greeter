979303f323e984572e6385eafc58ba8a18f199ee
Jan Friebe <friebe@odas.biz>
2018-01-04 08:06:47 +0100

    logik implementiert

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -2,14 +2,50 @@ import React from 'react';
 import {render} from 'react-dom';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      greeting: ''
+    }
+  }
+
+  onGreetingChange(event) {
+    this.setState({
+      greeting: event.target.value
+    });
+  }
+
+  onGreetClicked() {
+    alert(`Hello, ${this.state.greeting}`);
+  }
+
+  getCharsRemaining() {
+    return this.props.maxLength - this.state.greeting.length;
+  }
+
+  isGreetingValid() {
+    return this.state.greeting.length > 0 && this.getCharsRemaining() >= 0;
+  }
+
+
   render () {
+    let greetingValid = this.isGreetingValid();
     return (
       <div>
-        <p> Hello React!</p>
+        <input
+          value={this.state.greeting}
+          onChange={(e)=>this.onGreetingChange(e)}>
+        </input>
+        <span>{this.getCharsRemaining()}</span>
 
+          <button
+            disabled={console.log(!greetingValid)}
+            onClick={()=>this.onGreetClicked()}>
+            Senden
+          </button>
       </div>
     );
   }
 }
 
-render(<App/>, document.getElementById('app'));
+render(<App maxLength={10}/>, document.getElementById('app'));
